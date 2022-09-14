e29218f540f7c013319a451dbc54db27cb9972ea
Joseph DeWeese <rangerboy901@icloud.com>
2022-09-14 10:00:33 -0500

    Initial Commit

---
+++ SwiftUIReference/SwiftUIReferenceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIReferenceApp.swift
+//  SwiftUIReference
+//
+//  Created by Joseph Wil;liam DeWeese on 9/14/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIReferenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
